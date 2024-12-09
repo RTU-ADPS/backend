02f044ccf4d8c440423772adbdbd05d1280e739a
hoppjan <83405332+hoppjan@users.noreply.github.com>
2024-12-09 18:18:36 +0200

    Add routes to router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,18 @@
 import express from 'express';
-//import userRoutes from './users';
+import accountRoutes from './account';
+import accountHolderRoutes from './account-holder';
+import addressRoutes from './address';
+import debitCardRoutes from './debit-card';
+import paymentOptionRoutes from './payment-option';
+import transactionRoutes from './transaction';
 
 const router = express.Router();
 
-//router.use('/users', userRoutes);
+router.use('account', accountRoutes);
+router.use('account-holder', accountHolderRoutes);
+router.use('address', addressRoutes);
+router.use('debit-card', debitCardRoutes);
+router.use('payment-option', paymentOptionRoutes);
+router.use('transaction', transactionRoutes);
 
 export default router;
